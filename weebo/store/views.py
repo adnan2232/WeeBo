32c8eb0da013d391dca7afe247d4cc790c01913a
aa223285@gmail.com <aa2232783@gmail.com>
2021-03-23 21:56:53 +0530

    'main'

--- weebo/store/views.py
+++ weebo/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
 
 def store(request):
     if request.user.is_authenticated:
@@ -49,9 +50,7 @@ def updateItem(request):
     data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
-    print('Action: ',action)
-    print('productId: ',productId)
-
+    
     customer = request.user.customer
     product = Product.objects.get(id = productId)
     order,created = Order.objects.get_or_create(customer = customer, complete = False)
@@ -68,3 +67,28 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse('Item added successfully',safe=False)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    customer = request.user.customer
+    order,created = Order.objects.get_or_create(customer = customer, complete = False)
+    total = float(data["form"]["total"])
+    order.transaction_id = transaction_id
+
+    if total == float(order.get_cart_total) :
+        order.complete = True
+
+    order.save()
+
+    ShippingAddress.objects.create(
+        customer = customer,
+        order = order,
+        address = data["shipping"]["address"],
+        city = data["shipping"]["city"],
+        state = data["shipping"]["state"],
+        zipcode = data["shipping"]["zipcode"],
+    )
+
+    return JsonResponse('Payment successfully',safe=False)
